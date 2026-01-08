b54a8347b8d3f5fc67b07a74b2581957967ecbde
brunoandradegit <brunoazevedox@outlook.com>
2026-01-08 10:57:39 -0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,28 @@
+import { useState } from "react";
+
 export function App() {
+  const [count2, setCount2] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    Olá
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Aumenta 1</button>
+      <button
+        onClick={() => {
+          if (count == 0) {
+            setCount(0);
+            return;
+          }
+          setCount(count - 1);
+        }}
+      >
+        Diminui 1
+      </button>
+
+      <h1>{count2}</h1>
+      <button onClick={() => setCount2(count2 + 2)}>Aumenta 2</button>
+      <button onClick={() => setCount2(count2 - 2)}>Diminui 2</button>
     </>
-  )
+  );
 }
-
-
-// import { useState } from "react"
-
-// function App() {
-//   const [count2, setCount2] = useState(0)
-//   const [count, setCount] = useState(0)
-
-
-//   return (
-//     <>
-//     <h1>{count}</h1>
-//       <button onClick={() => setCount(count+1) }>
-//         Count +1
-//         </button>
-//         <button onClick={() => setCount(count-1) }>
-//           Count -1
-//         </button>
-//     <div>
-//        <button onClick={() => setCount2(count2+2) }>
-//         Count
-//         </button>
-//         <button onClick={() => setCount2(count2-2) }>
-//           Count -2
-//         </button>
-//         <h1>{count2}</h1>
-    
-      
-   
-//     </div>
-//     </>
-//   )
-// }
-
-// export default App
