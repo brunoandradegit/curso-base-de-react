b3b921f43cbe3c50357a79aa8f1b672eb6a067d2
brunoandradegit <brunoazevedox@outlook.com>
2026-01-13 12:01:44 -0300

    feat: negative

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,38 @@ export function App() {
   const [count2, setCount2] = useState(0);
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Aumenta 1</button>
-      <button
-        onClick={() => {
-          if (count == 0) {
-            setCount(0);
-            return;
-          }
-          setCount(count - 1);
-        }}
-      >
-        Diminui 1
-      </button>
+  function updateCount(value: number){
+    if(value < 0) {
+      if((count - (value * -1)) < 0) {
+        return
+      }
 
-      <h1>{count2}</h1>
-      <button onClick={() => setCount2(count2 + 2)}>Aumenta 2</button>
-      <button onClick={() => setCount2(count2 - 2)}>Diminui 2</button>
-    </>
+      setCount(count - value)
+    }
+
+    setCount(count + value)
+
+  }
+
+  return (
+     <div>
+      {count}
+      <button onClick={() => updateCount(1)}>
+      +1
+      </button>
+      <button onClick={() => updateCount(-1)}>
+      -1
+      </button>
+      <button onClick={() => updateCount(2) }>
+        +2
+      </button>
+      <button onClick={() => updateCount(-2)}>
+        -2
+      </button>
+     </div>
+     
   );
 }
+
+
+
